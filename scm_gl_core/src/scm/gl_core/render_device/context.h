46d6a5613a1d69cfc51f923a4417b858bdc149d1
Lucky Chandrautama <gejo4207@artemis.medien.uni-weimar.de>
2020-03-06 19:59:44 +0100

    update gl headers and expose gldrawElementsInstancedBaseVertex

--- scm_gl_core/src/scm/gl_core/render_device/context.h
+++ scm_gl_core/src/scm/gl_core/render_device/context.h
@@ -239,6 +239,7 @@ public:
 
     void                        draw_arrays(const primitive_topology in_topology, const int in_first_index, const int in_count);
     void                        draw_elements(const int in_count, const int in_start_index = 0, const int in_base_vertex = 0);
+    void                        draw_elements_instanced(const int in_count, const int in_start_index = 0, const int in_instance_count = 1, const int in_base_vertex = 0);
 
     bool                        make_resident(const buffer_ptr&     in_buffer,
                                               const access_mode     in_access);
